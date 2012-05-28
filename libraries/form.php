b59ca675d9a6e5bd5d7c1dbf517e18b175427018
Scott Travis <scott@swt83.com>
2012-05-28 10:27:17 -0400

    Improvements.

--- libraries/form.php
+++ libraries/form.php
@@ -1,12 +1,11 @@
 <?php
 
 /**
- * A LaravelPHP package for working w/ form models.
+ * A model to facilitate working w/ forms in LaravelPHP.
  *
  * @package    FormModel
- * @author     Shawn McCool
  * @author     Scott Travis <scott.w.travis@gmail.com>
- * @link       http://github.com/swt83/laravel-form
+ * @link       http://github.com/swt83/laravel-form-model
  * @license    MIT License
  */
 
@@ -61,102 +60,110 @@ class FormModel
 		return static::$validation->passes();
 	}
 	
-	protected static function serialize()
+	/**
+	 * Serialize data and store in session.
+	 */
+	protected static function push()
 	{
-		Session::put(get_called_class(), Crypter::encrypt(serialize(static::$data)));
+		Session::put(get_called_class(), serialize(static::$data));
 	}
 	
-	protected static function unserialize()
+	/**
+	 * Unserialize session and populate data array.
+	 */
+	protected static function pull()
 	{
 		if (Session::has(get_called_class()))
 		{
-			static::$data = unserialize(Crypter::decrypt(Session::get(get_called_class())));
+			static::$data = unserialize(Session::get(get_called_class()));
 		}
 	}
 	
-	public static function remember($fields = null, $input = null)
+	/**
+	 * Save data array.
+	 */
+	public static function remember()
 	{
-		// check error
-		if (!is_array($fields) and !is_null($fields))
-		{
-			return false;
-		}
-		
-		// get fields
-		if (is_null($fields))
-		{
-			$fields = array_keys(Input::all());
-		}
-
-		// get values
-		if (is_null($input))
-		{
-			$input = Input::all();
-		}
-
-		// load existing
-		if (empty(static::$data))
-		{
-			static::unserialize();
-		}
-
-		// save values
-		foreach ($fields as $field)
-		{
-			if (Input::has($field))
-			{
-				static::$data[$field] = Input::get($field);
-			}
-			else
-			{
-				static::$data[$field] = '';
-			}
-		}
-
-		// serialize
-		static::serialize();
+		// remember data
+		static::push();
 	}
 	
+	/**
+	 * Unsave data array.
+	 */
 	public static function forget()
 	{
+		// forget data
 		Session::forget(get_called_class());
 	}
 	
+	/**
+	 * Fill data fields.
+	 */
+	public static function fill($array)
+	{
+		foreach ($array as $field => $value)
+		{
+			static::$data[$field] = $value;
+		}
+	}
+	
+	/**
+	 * Set data field.
+	 */
+	public static function set($field, $value)
+	{
+		// set new value
+		static::$data[$field] = $value;
+	}
+	
+	/**
+	 * Check for data field.
+	 */
 	public static function has($field)
 	{
+		// return has field
 		return isset(static::$data[$field]) and !empty(static::$data[$field]);
 	}
-
+	
+	/**
+	 * Get data field.
+	 */
 	public static function get($field, $default = null)
 	{
-		if (empty(static::$data))
-		{
-			static::unserialize();
-		}
-		
+		// return value
 		return static::has($field) ? static::$data[$field] : $default;
 	}
 	
+	/**
+	 * Get all data fields.
+	 */
 	public static function all()
 	{
+		// return data array
 		return static::$data;
 	}
 	
+	/**
+	 * Get best field value.
+	 */
 	public static function populate($field, $default = null)
 	{
-		if (empty(static::$data))
-		{
-			static::unserialize();
-		}
-
+		// return best value
 		return Input::old($field, static::get($field, $default));
 	}
 	
+	/**
+	 * Get validation object.
+	 */
 	public static function validation()
 	{
 		return static::$validation;
 	}
 	
+	/**
+	 * Get errors array.
+	 */
 	public static function errors()
 	{
 		$errors = Session::get('errors');
@@ -171,6 +178,9 @@ class FormModel
 		}
 	}
 	
+	/**
+	 * Get field error.
+	 */
 	public static function error($field, $default = null)
 	{
 		$errors = Session::get('errors');
